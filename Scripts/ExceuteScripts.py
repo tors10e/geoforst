0ab43870efedcae6152c77dc96e8fc433062685e
torstenernst <torstenernst@localhost>
2012-12-16 13:03:29 -0900

    Cleaned up FSP directory, removed unneeded files, and reorganized where possible.

--- Scripts/ExceuteScripts.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-qry = open('test.sql', 'r').read()
-conn = sqlite3.connect('/Users/torsten/Documents/GIS/FSP/FSP.sqlite')
-c = conn.cursor()
-c.execute(qry)
-conn.commit()
-c.close()
-conn.close()
