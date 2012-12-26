0f44cf60473f5c75a91a8d580750d94856cc4b0f
torstenernst <tors10e@users.noreply.github.com>
2012-12-26 09:36:32 -0900

    Added permissions for users to select and update sequences for editing.

--- Scripts/DDL/pg/grants.sql
+++ Scripts/DDL/pg/grants.sql
@@ -3,7 +3,7 @@ ALTER ROLE cernst ENCRYPTED PASSWORD 'md50c0718d13edf4bb454f99c6d45c97754'
    VALID UNTIL '2013-12-31 00:00:00';
    
 create user ternst login inherit;
-ALTER ROLE ternst ENCRYPTED PASSWORD 'md5fdf030db6faa24f25fea2fd8bd1ac629'
+ALTER ROLE ternst ENCRYPTED PASSWORD 'md5f5f1f55bac05126a41f6d3e9d371fd5b'
    VALID UNTIL '2013-12-31 00:00:00';
 
 create role fsp_read;
@@ -21,4 +21,6 @@ GRANT ALL ON DATABASE fsp TO fsp_admin;
 
 grant select on all tables in schema public to fsp_read;
 grant select, update, insert, delete on all tables in schema public to fsp_edit;
+grant usage, select, update on all sequences in schema public to fsp_edit;
 grant all on all tables in schema public to fsp_admin;
+grant usage, select, update on all sequences in schema public to fsp_admin;
