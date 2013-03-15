49bf5cc727cca3433ea2e4ae3fded22ea80db454
torstenernst <tors10e@users.noreply.github.com>
2013-03-15 15:49:28 -0800

    Added QC scripts for spatiotemporal consistency checks.

---
+++ Scripts/adhoc/20130202_reset_startdate_trg.sql
@@ -0,0 +1,26 @@
+﻿-- Function: public.reset_stand_area_startdate()
+
+--DROP TRIGGER trg_reset_stand_area_startdate ON public.stand_area;
+ 
+--DROP FUNCTION public.reset_stand_area_startdate();
+
+CREATE OR REPLACE FUNCTION public.reset_startdate()
+  RETURNS trigger AS
+$$
+BEGIN
+  NEW.start_date = now();
+  RETURN NEW;
+END;
+$$
+  LANGUAGE plpgsql;
+  
+ALTER FUNCTION public.reset_startdate()
+  OWNER TO postgres;
+
+
+
+CREATE TRIGGER trg_reset_stand_area_startdate
+  BEFORE UPDATE
+  ON public.stand_area
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.reset_startdate();
