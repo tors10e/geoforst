bb8ee6f53624c20875a4857efd0e086e8ef3a97d
torstenernst <torstenernst@localhost>
2013-09-08 23:23:03 -0800

    Added editing form and add new plot data.

--- geoweb/geoapp/forms.py
+++ geoweb/geoapp/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from geoapp.models import ForestInventoryPlot
+from geoapp.models import ForestInventoryPlot, ForestInventoryData
 
 class InventoryPlotForm(ModelForm):
     class Meta:
@@ -7,5 +7,10 @@ class InventoryPlotForm(ModelForm):
         fields = ['forestinventoryplot_id', 'plot_area_ft2', 'plot_radius_ft','plot_length_x_ft',
                   'plot_length_y_ft', 'plot_geometry', 'elevation', 'position_description', 
                   'plot_create_date']
+
+class InventoryDataForm(ModelForm):
+    class Meta:
+        model = ForestInventoryData
+        fields = ['forestinventorydata_id', 'forestinventoryplot', 'tree', 'collection_date', 'species', 'dbh_in', 'height_ft']
         
     
