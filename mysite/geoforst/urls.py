3ca6db1162b08a96795c9a90a31a89dd581fcb92
torstenernst <tors10e@users.noreply.github.com>
2013-09-26 21:48:18 -0800

    Removing "mysite" test area.

--- mysite/geoforst/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from geoforst import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.StandAreaView.as_view(), name='StandAreaVw'),
-)
--- mysite/geoforst/views.py
+++
@@ -1,8 +0,0 @@
-from django.views import generic
-
-from geoforst.models import StandArea
-
-class StandAreaView(generic.ListView):
-    model = StandArea
-    template_name = 'geoforst/index.html'
-    context_object_name = 'test_context_object_name'# Create your views here.
