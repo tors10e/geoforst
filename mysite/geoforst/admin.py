ba1243d8ddf3d8dd0d62f1dd28428241ef13f306
torstenernst <torstenernst@localhost>
2013-09-26 21:48:18 -0800

    Removing "mysite" test area.

--- mysite/geoforst/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.gis import admin
-from geoforst.models import StandArea
-
-#admin.site.register(WorldBorder, admin.GeoModelAdmin)
-admin.site.register(StandArea, admin.OSMGeoAdmin)
