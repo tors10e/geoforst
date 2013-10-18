8ec988e74ffa3d50a114e2acfbea77b95a14f1fb
torstenernst <tors10e@users.noreply.github.com>
2013-10-17 22:05:22 -0800

    Updated forms to handle  updates to models.

--- geoweb/geoapp/forms.py
+++ geoweb/geoapp/forms.py
@@ -5,14 +5,17 @@ from geoapp.models import ForestInventoryPlot, ForestInventoryData
 class InventoryPlotForm(ModelForm):
     class Meta:
         model = ForestInventoryPlot
-        fields = ['forestinventoryplot_id', 'plot_area_ft2', 'plot_radius_ft','plot_length_x_ft',
-                  'plot_length_y_ft', 'plot_geometry', 'elevation', 'position_description', 
+        fields = ['forestinventoryplot_id', 'plot_area', 'plot_area_unit','plot_radius', 'radius_unit', 'plot_xlength', 'plot_xlength_unit',
+                  'plot_ylength', 'plot_ylength_unit', 'plot_geometry', 'elevation', 'elevation_unit','position_description', 
                   'plot_create_date']
+        exclude = ('created_by',)
+        
         
 class InventoryDataForm(ModelForm):
     class Meta:
         model = ForestInventoryData
         fields = ['forestinventorydata_id', 'forestinventoryplot', 'tree', 'collection_date', 
-                  'species', 'dbh_in', 'height_ft']
+                  'tree_species', 'dbh', 'height']
+        exclude = ('created_by',)
         
     
