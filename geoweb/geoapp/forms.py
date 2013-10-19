bcf9881109fd1fb620c3b4fcde193152cfdd8fc5
torstenernst <tors10e@users.noreply.github.com>
2013-10-18 22:51:12 -0800

    Created style sheets to style application. Fixed error with foreign key fields not showing in table displays for plots. Reworked urls to use a more natural naming convention.

--- geoweb/geoapp/forms.py
+++ geoweb/geoapp/forms.py
@@ -5,17 +5,12 @@ from geoapp.models import ForestInventoryPlot, ForestInventoryData
 class InventoryPlotForm(ModelForm):
     class Meta:
         model = ForestInventoryPlot
-        fields = ['forestinventoryplot_id', 'plot_area', 'plot_area_unit','plot_radius', 'radius_unit', 'plot_xlength', 'plot_xlength_unit',
-                  'plot_ylength', 'plot_ylength_unit', 'plot_geometry', 'elevation', 'elevation_unit','position_description', 
-                  'plot_create_date']
         exclude = ('created_by',)
         
         
 class InventoryDataForm(ModelForm):
     class Meta:
         model = ForestInventoryData
-        fields = ['forestinventorydata_id', 'forestinventoryplot', 'tree', 'collection_date', 
-                  'tree_species', 'dbh', 'height']
         exclude = ('created_by',)
         
     
--- geoweb/geoapp/views.py
+++ geoweb/geoapp/views.py
@@ -7,10 +7,6 @@ from django.core.urlresolvers import reverse
 from django.contrib import auth
 from django.views.generic.edit import CreateView
 
-# Does not get referenced.
-class IndexView(ListView):
-    template_name = 'geoapp/index.html'
-    context_object_name = 'latest_geoapp_list'
 
 def Index(request):
     return render_to_response('geoapp/index.html', {})
@@ -24,6 +20,7 @@ def Home(request):
 class InventoryPlotListView(ListView):
     template='geoapp/forestinventoryplot_list.html'
     context_object_name='inventory_plots'
+    
     def get_queryset(self):
         return ForestInventoryPlot.objects.filter(created_by=self.request.user)
 
@@ -32,26 +29,26 @@ class InventoryPlotDetailView(DetailView):
     template_name = 'geoapp/forestinventoryplot_detail.html'
     context_object_name = 'plot_detail'
     
+    def get_queryset(self):
+        return ForestInventoryPlot.objects.filter(created_by=self.request.user)
+    
 class ForestInventoryPlotCreate(CreateView):
     form_class = InventoryPlotForm
     model = ForestInventoryPlot
     
+    # Return to the inventory plot list when done creating a plot.
     def get_success_url(self):
         return reverse('geoapp:inventory_plot_list')
     
+    # Set created_by to the current user.
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super(ForestInventoryPlotCreate, self).form_valid(form)
- 
-def InventoryPlotAdd(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = InventoryPlotForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            form.save()
-            return HttpResponseRedirect('/geoapp/inventory_plot/') # Redirect after POST
-    else: 
-        form = InventoryPlotForm() # An unbound form
-        return render(request, 'geoapp/forestinventoryplot_add.html', {'form': form})
+    
+    # Set default form values.
+    def get_initial(self):
+        return {'plot_geometry':1, 'plot_area_unit':1, 'plot_radius_unit':1}
+
     
 def InventoryPlotEdit(request, forestinventoryplot_id):
     plot = get_object_or_404(ForestInventoryPlot, pk=forestinventoryplot_id)
@@ -59,7 +56,7 @@ def InventoryPlotEdit(request, forestinventoryplot_id):
         form = InventoryPlotForm(request.POST or None, instance=plot)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/geoapp/inventory_plot/')
+        return HttpResponseRedirect('/geoapp/inventory_plot/')
     else:
         form = InventoryPlotForm(instance=plot)
         return render(request, 'geoapp/forestinventoryplot_update.html', {'form': form})
@@ -67,16 +64,16 @@ def InventoryPlotEdit(request, forestinventoryplot_id):
 def InventoryPlotDelete(request, pk):
     ForestInventoryPlot.objects.get(forestinventoryplot_id=pk).delete()
     return HttpResponseRedirect('/geoapp/inventory_plot/')
-         
+
 def InventoryDataAdd(request, forestinventoryplot_id=1):
-    if request.method == 'POST': # If the form has been submitted...
-        form = InventoryDataForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    if request.method == 'POST': 
+        form = InventoryDataForm(request.POST) 
+        if form.is_valid(): 
             new_data = form.save()
             return HttpResponseRedirect(reverse('geoapp:plot_detail', kwargs={'pk':new_data.forestinventoryplot_id}))
     else: 
-        # Get a form and populate with forestinventoryplotid if it is given.
-        initial_data = {'forestinventoryplot' : forestinventoryplot_id}
+        # Get a form and populate with forestinventoryplotid and default units.
+        initial_data = {'forestinventoryplot' : forestinventoryplot_id, 'dbh_unit':3, 'height_unit':1}
         form = InventoryDataForm(initial=initial_data) 
     return render(request, 'geoapp/forestinventorydata_add.html', {'form': form})
 
