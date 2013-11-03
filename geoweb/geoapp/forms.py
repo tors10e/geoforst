44b9a34ca65d51ef08141b00d24d84c729cb4dc2
torstenernst <tors10e@users.noreply.github.com>
2013-11-03 09:33:52 -0900

    Updated plot inventory to include plot number. Added latitude and longitude fields for inventory plot as a stop gap measure while finding a solution to edit point fields in django forms. Updated inventory plot template.

--- geoweb/geoapp/forms.py
+++ geoweb/geoapp/forms.py
@@ -1,14 +1,21 @@
-from django.forms import ModelForm
+from django.forms import Textarea
 from geoapp.models import ForestInventoryPlot, ForestInventoryData
+from django.contrib.gis import forms 
+from geoapp import custom_widgets 
+from django.contrib.gis.forms.widgets import OpenLayersWidget
+from django.forms.widgets import TextInput
 
-
-class InventoryPlotForm(ModelForm):
+class InventoryPlotForm(forms.ModelForm):
     class Meta:
         model = ForestInventoryPlot
-        exclude = ('created_by',)
-        
-        
-class InventoryDataForm(ModelForm):
+        exclude = ('created_by','geometry') # Geometry field is temporarily suspended and replaced with lat/long.
+        widgets = {
+            'position_description': Textarea(attrs={'cols': 80, 'rows': 5}),
+#            'geometry': custom_widgets.LatLongField(attrs={'size'   :50}),
+            'geometry':TextInput(attrs={'size':75}),
+#            'geometry':custom_widgets.LatLongField(),
+        }       
+class InventoryDataForm(forms.ModelForm):
     class Meta:
         model = ForestInventoryData
         exclude = ('created_by',)
