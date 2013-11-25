92329df5b298bb585567e0a2b9523cb9c9d93a94
torstenernst <tors10e@users.noreply.github.com>
2013-11-24 17:46:07 -0900

    Updated scripts  to go from dev to prod and vice versa to generate maps with new names.

--- Scripts/DevToProd/sed_qgsDevToProd.sh
+++ Scripts/DevToProd/sed_qgsDevToProd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Converts the development multiuser edition map to generate the production mulituser map.
 # This script should be run from FSP home.
 
-sed -E -f Scripts/DevToProd/sedrules_qgsDevToProd.txt Maps/FSP_PGLocal.qgs > Maps/FSP_PG.qgs
+sed -E -f Scripts/DevToProd/sedrules_qgsDevToProd.txt Maps/GForst_ME_Dev.qgs > Maps/GForst_ME.qgs
--- Scripts/DevToProd/sed_qgsProdToDev.sh
+++ Scripts/DevToProd/sed_qgsProdToDev.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Creates development version of multiuser edition from production map.
 # This script should be run from FSP home.
 
-sed -E -f Scripts/DevToProd/sedrules_qgsProdToDev.txt Maps/FSP_PG.qgs > Maps/FSP_PGLocal.qgs
+
+sed -E -f Scripts/DevToProd/sedrules_qgsProdToDev.txt Maps/GForst_ME.qgis > Maps/FSP_PGLocal.qgs
