0f48694ff3e2ab260c852f02ac578e00aa79b9df
torstenernst <torstenernst@localhost>
2013-11-28 12:35:38 -0900

    Added script to deploy Geoforst webpages to torstenernst.com/geoforst.

--- Scripts/Deployment/package_gforst_lite.sh
+++ Scripts/Deployment/package_gforst_lite.sh
@@ -3,13 +3,19 @@
  # Create directory structure.
  mkdir -p ~/temp/GForst_Lite/Maps
  mkdir -p ~/temp/GForst_Lite/Data
-  
+ mkdir -p ~/temp/GForst_Lite/Forms
+   
  # Copy files to new directories.
- cp ../../Maps/GForst_Lite.qgs ~/temp/GForst_lite/Maps/
- cp ../../Data/FSP2.sqlite ~/temp/GForst_Lite/Data
+ cp $GEOFORST_HOME/Maps/GForst_Lite.qgs ~/temp/GForst_lite/Maps
+ cp $GEOFORST_HOME/Data/FSP2.sqlite ~/temp/GForst_Lite/Data
+ cp $GEOFORST_HOME/Forms/* ~/temp/GForst_lite/Forms
+ ls -la ~/temp/GForst*
  
  # Confirm that there are no errors with the map and data source.
- open ~/temp/GForst_Lite/Maps/GForst_Lite.qgs   
+ # open ~/temp/GForst_Lite/Maps/GForst_Lite.qgs   
  
  # Create a zip package.
- zip -r ~/temp/GForst_Lite.zip ~/temp/GForst_Lite/ 
+ cd ~/temp # The path to the diretory to be zipped must be relative to prevent zip from including the full path in the output.
+ zip -ru $GEOFORST_HOME/www/WebContent/packages/0.1/GForst_Lite_v0.1.zip  ./GForst_Lite/ 
+ 
+ 
