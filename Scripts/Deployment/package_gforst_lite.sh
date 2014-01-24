c4c74d8a83e1f5bf08acff4591b896e22267ff9b
torstenernst <tors10e@users.noreply.github.com>
2014-01-23 22:40:49 -0900

    Added GeoForst_ME to web site.

--- Scripts/Deployment/package_gforst_lite.sh
+++ Scripts/Deployment/package_gforst_lite.sh
@@ -4,11 +4,14 @@
  mkdir -p ~/temp/GForst_Lite/Maps
  mkdir -p ~/temp/GForst_Lite/Data
  mkdir -p ~/temp/GForst_Lite/Forms
-   
+ mkdir -p ~/temp/GForst_ME
+ 
  # Copy files to new directories.
  cp $GEOFORST_HOME/Maps/GForst_Lite.qgs ~/temp/GForst_lite/Maps
  cp $GEOFORST_HOME/Data/FSP2.sqlite ~/temp/GForst_Lite/Data
  cp $GEOFORST_HOME/Forms/* ~/temp/GForst_lite/Forms
+ cp $GEOFORST_HOME/Maps/GForst_ME.qgs ~/temp/GForst_ME
+ 
  ls -la ~/temp/GForst*
  
  # Confirm that there are no errors with the map and data source.
@@ -17,5 +20,6 @@
  # Create a zip package.
  cd ~/temp # The path to the diretory to be zipped must be relative to prevent zip from including the full path in the output.
  zip -ru $GEOFORST_HOME/www/WebContent/packages/0.1/GForst_Lite_v0.1.zip  ./GForst_Lite/ 
+ zip -ru $GEOFORST_HOME/www/WebContent/packages/0.1/GForst_ME_v0.1.zip ./GForst_ME/ 
  
  
