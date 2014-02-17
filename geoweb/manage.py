a8824b53bbe03f345f8ceeaf08161e3ff72899fe
torstenernst <torstenernst@localhost>
2014-02-17 09:12:22 -0900

    Made configuration environment sensitive and self configuring.

--- geoweb/manage.py
+++ geoweb/manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import socket
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoweb.settings")
-
+    if socket.gethostname() == 'geoweb':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoweb.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geoweb.settings_dev")
+        
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
