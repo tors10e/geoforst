5cfb44bbcdebb992d5a5a9a2a9dfdba0d6d7b8bb
torstenernst <tors10e@users.noreply.github.com>
2014-04-03 07:17:38 -0800

    Updated deployment scripts by organizing them better and collapsing them into fewer scripts.

--- Scripts/Deployment/deployGForstLiteToWeb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Copy geoforst web site to torstenernst.com/geoforst.
-echo "Copying from $GEOFORST_HOME/www/WebContent to torstenernst.com/geoforst."
-scp -r $GEOFORST_HOME/www/WebContent/* ternst@vector.xyxx.com:/home/ternst/www/torstenernst/geoforst
-echo "Copying complete...."
--- Scripts/Deployment/sed_qgsDevToProd.sh
+++ Scripts/Deployment/sed_qgsDevToProd.sh
@@ -2,4 +2,4 @@
 # Converts the development multiuser edition map to generate the production mulituser map.
 # This script should be run from FSP home.
 
-sed -E -f $GEOFORST_HOME/Scripts/DevToProd/sedrules_qgsDevToProd.txt $GEOFORST_HOME/Maps/GForst_ME_Dev.qgs > $GEOFORST_HOME/Maps/GForst_ME.qgs
+sed -E -f $GEOFORST_HOME/Scripts/Deployment/sedrules_qgsDevToProd.txt $GEOFORST_HOME/Maps/GForst_ME_Dev.qgs > $GEOFORST_HOME/Maps/GForst_ME.qgs
