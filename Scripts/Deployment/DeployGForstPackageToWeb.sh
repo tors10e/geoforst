ba1fd3a66c6a60becdc67680328064cd5e967f32
torstenernst <tors10e@users.noreply.github.com>
2014-04-03 12:15:22 -0800

    Added automatic insert of last updated date to html page of torstenernst.com/geoforst so that you can tell if you have the most up to date package.

--- Scripts/Deployment/DeployGForstPackageToWeb.sh
+++ Scripts/Deployment/DeployGForstPackageToWeb.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#!/bin/sh
-
  # Create directory structure.
  mkdir -p ~/temp/GForst_Lite/Maps
  mkdir -p ~/temp/GForst_Lite/Data
@@ -12,6 +10,7 @@
  
  echo "Copying files to temp directories..."
  # Copy files to new directories.
+ cp -r  $GEOFORST_HOME/www/WebContent/ ~/temp/gf_deployment # Copy website to temp.
  cp $GEOFORST_HOME/Maps/GForst_Lite.qgs ~/temp/GForst_lite/Maps
  cp $GEOFORST_HOME/Data/FSP2.sqlite ~/temp/GForst_Lite/Data
  cp $GEOFORST_HOME/Forms/* ~/temp/GForst_lite/Forms
@@ -21,19 +20,25 @@
  
  echo "Zipping the files..."
  # Create a zip package.
- rm $GEOFORST_HOME/www/WebContent/packages/0.1/* # Clean the directory first since things weren't getting overwritten.
+ rm ~/temp/gf_deployment/packages/0.1/* # Clean the directory first since things weren't getting overwritten.
  cd ~/temp # The path to the directory to be zipped must be relative to prevent zip from including the full path in the output.
- zip -ru $GEOFORST_HOME/www/WebContent/packages/0.1/GForst_Lite_v0.1.zip  ./GForst_Lite/ 
- zip -ru $GEOFORST_HOME/www/WebContent/packages/0.1/GForst_ME_v0.1.zip ./GForst_ME/ 
+  
+ # Update date on web page.
+ currDate=$(date +"%m-%d-%Y")
+ sed -i '' 's/\[DATE\]/'$currDate/g  ~/temp/gf_deployment/index.html
+ 
+ zip -ru ~/temp/gf_deployment/packages/0.1/GForst_Lite_v0.1.zip  ./GForst_Lite/ 
+ zip -ru ~/temp/gf_deployment/packages/0.1/GForst_ME_v0.1.zip ./GForst_ME/ 
  
  # Copy geoforst web site to torstenernst.com/geoforst.
 echo "Copying from $GEOFORST_HOME/www/WebContent to torstenernst.com/geoforst..."
-scp -r $GEOFORST_HOME/www/WebContent/* ternst@vector.xyxx.com:/home/ternst/www/torstenernst/geoforst
+scp -r ~/temp/gf_deployment/* ternst@vector.xyxx.com:/home/ternst/www/torstenernst/geoforst
 echo "Copying complete."
  
  echo "Cleaning up temp files..."
- rm -r ~/temp/GForst_lite/*
- rm -r ~/temp/GForst_ME/*
- 
+ rm -r ~/temp/GForst_lite
+ rm -r ~/temp/GForst_ME
+ rm -r ~/temp/gf_deployment
+  
  echo "Packaging and deploying complete."
  
