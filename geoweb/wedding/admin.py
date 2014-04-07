337f00407ec7325fbffc8d4441464ac9ac54b73d
torstenernst <torstenernst@localhost>
2014-04-06 22:37:15 -0800

    Added new wedding application.

---
+++ geoweb/wedding/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from wedding.models import Present
+
+admin.site.register(Present)
+# Register your models here.
---
+++ geoweb/wedding/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from wedding import views
+
+urlpatterns = patterns('',
+    url(r'^registry/$', views.PresentList.as_view() , name = 'present-list'),
+    url(r'^registry/(?P<pk>\d+)/edit/$', views.PresentUpdate.as_view() , name = 'present-edit'),
+    url(r'^(?P<present_id>\d+)$', views.UpdatePresentStatus, name = 'toggle-purchased')
+  )
