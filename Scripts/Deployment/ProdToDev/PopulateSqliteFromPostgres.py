193c01abf195e83b136e2af8389c650a2041e9e8
torstenernst <tors10e@users.noreply.github.com>
2014-08-11 13:16:39 +1000

    Added explicit geometry type definition for each spatial table so that import to sqlite also registers the geometry type (POINT, LINESTRING, etc.).

--- Scripts/Deployment/ProdToDev/PopulateSqliteFromPostgres.py
+++ Scripts/Deployment/ProdToDev/PopulateSqliteFromPostgres.py
@@ -1,66 +1,77 @@
 import os
+import getpass
 
 host = "geo.torstenernst.com"
 port = 5432
 username = "ternst"
-password = "Black1ce!"
+password = ""
 database = "fsp"
 outputPath = "~/temp/"
 outputFile = "geoforst.sqlite"
-tables = ["public.cultural_type",
-          "public.unit_concentration_type",
-          "public.forest_age_type",
-          "public.harvest_type",
-          "public.ordinal_magnitude_type",
-          "public.plot_geometry_type",
-          "public.ordinal_sufficiency_type",
-          "public.species_tree_type",
-          "public.unit_length_type",
-          "public.activity_type",
-          "public.unit_area_type",
-          "public.recreation_type",
-          "public.habitat_enhancement_type",
-          "public.road_access_status_type",
-          "public.forest_type",
-          "public.road_surface_type",
-          "public.road_type",
-          "public.severity_type",
-          "public.stand_type",
-          "public.stocking_type",
-          "public.task_status_type",
-          "public.unit_type",
-          "public.water_type",
-          "public.trail_status_type",
-          "public.trail_type",
-          "public.activity_area", 
-          "public.burn_compartment", 
-          "public.control_point", 
-          "public.cultural_point", 
-          "public.firebreak_line", 
-          "forest_inventory_plot", 
-          "public.habitat_enhancement_area", 
-          "public.habitat_enhancement_point", 
-          "public.land_area",
-          "public.recreation_point", 
-          "public.road_line", 
-          "public.soil_test", 
-          "public.stand_area", 
-          "public.stream",
-          "public.streamside_management_zone", 
-          "public.trail", 
-          "public.tree" ,
-          "public.water_point",
-          "public.planned_activity",
-          "public.soil_test_recommendation",
-          "public.stand_description",
-          "public.stand_status",
-          "public.tree_status"]
+spatialTables = [
+    ["activity_area", "POLYGON"],
+    ["burn_compartment", "POLYGON"],
+    ["control_point", "POINT"],
+    ["cultural_point", "POINT"],
+    ["firebreak_line", "LINESTRING"],
+    ["forest_inventory_plot", "POLYGON"],
+    ["habitat_enhancement_area", "POLYGON"],
+    ["habitat_enhancement_point", "POINT"],
+    ["land_area", "POLYGON"],
+    ["recreation_point", "POINT"],
+    ["road_line", "LINESTRING"],
+    ["soil_test", "POINT"],
+    ["stand_area", "POLYGON"],
+    ["stream", "LINESTRING"],
+    ["streamside_management_zone", "POLYGON"],
+    ["trail", "LINESTRING"],
+    ["tree" , "POINT"],
+    ["water_point", "POINT"],
+    ["unit_concentration_type"],
+    ["forest_age_type"],
+    ["harvest_type"],
+    ["ordinal_magnitude_type"],
+    ["plot_geometry_type"],
+    ["ordinal_sufficiency_type"],
+    ["species_tree_type"],
+    ["unit_length_type"],
+    ["activity_type"],
+    ["unit_area_type"],
+    ["recreation_type"],
+    ["habitat_enhancement_type"],
+    ["road_access_status_type"],
+    ["forest_type"],
+    ["road_surface_type"],
+    ["road_type"],
+    ["severity_type"],
+    ["stand_type"],
+    ["stocking_type"],
+    ["task_status_type"],
+    ["unit_type"],
+    ["water_type"],
+    ["trail_status_type"],
+    ["trail_type"],
+    ["planned_activity"],
+    ["soil_test_recommendation"],
+    ["stand_description"],
+    ["stand_status"],
+    ["tree_status"]
+]
 
-#password = input("Please enter password for user %s") %(username)
+
+username = raw_input("Please enter username for database %s on %s: " %(database, host))
+password = getpass.getpass("Please enter password for user %s: " %(username))
 print("Loading...")
-for table in tables:
-   commandString = "ogr2ogr -f SQLite -overwrite  %s%s  PG:\"host=%s user=%s dbname=%s password=%s port=%s\" %s" %(outputPath, outputFile, host, username, database, password, port, table)
-   print("Loading table " + table)
+for table in spatialTables:
+   connectString = "ogr2ogr -f SQLite -overwrite  %s%s  PG:\"host=%s user=%s dbname=%s password=%s port=%s\"" %(outputPath, outputFile, host, username, database, password, port)
+   selectString = "-sql \"select * from %s;\"" %(table[0])
+   nameString = "-nln %s" %(table[0])
+   if table.__len__() > 1 :
+       geomTypeString = "-nlt %s" %(table[1])
+   else: geomTypeString = ""
+   commandString = connectString + " " + selectString + " " + nameString + " " + geomTypeString
+   print("Loading table " + table[0])
+   #print(commandString)
    os.system(commandString)
 
 print ("Loading complete!")
