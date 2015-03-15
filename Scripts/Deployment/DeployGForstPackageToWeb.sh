400a19a538013f9bbe75fd152cf2c6c4dfbf1166
torstenernst <torstenernst@localhost>
2015-03-15 16:30:36 +1000

    Wrapped deployment scripts into one file.

--- Scripts/Deployment/DeployGForstPackageToWeb.sh
+++ Scripts/Deployment/DeployGForstPackageToWeb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 PACKAGE_VERSION="0.1"
-SOURCE_DIR="/home/ubuntu/downloads/fsp"
-STAGING_DIR="/home/ubuntu/temp"
-WEB_DIR="/var/www"
- 
-cd /home/ubuntu/downloads/
-sudo rm -rf /home/ubuntu/downloads/fsp
-hg clone https://torstenernst@bitbucket.org/torstenernst/fsp
+SOURCE_DIR="~/Downloads/fsp"
+STAGING_DIR="~/temp"
+WEB_DIR="$WEB_DIR"
 
+# Get latest source code.
+sudo rm -rf $SOURCE_DIR
+sudo rm -rf $STAGING_DIR/*
+hg clone -r tip https://torstenernst@bitbucket.org/torstenernst/fsp $SOURCE_DIR
 
  # Create directory structure.
  sudo mkdir -p $STAGING_DIR/GForst_Lite/Maps
@@ -23,16 +23,16 @@ hg clone https://torstenernst@bitbucket.org/torstenernst/fsp
 
  echo "Copying files to temp directories..."
  # Copy files to new directories.
- sudo cp -r  $SOURCE_DIR/www/WebContent/* $STAGING_DIR/gf_deployment 
+ sudo cp -r  $SOURCE_DIR/geoweb/* $STAGING_DIR/gf_deployment 
  
  sudo cp $SOURCE_DIR/Maps/GForst_Lite.qgs $STAGING_DIR/GForst_Lite/Maps
  sudo cp $SOURCE_DIR/Data/geoforst.sqlite $STAGING_DIR/GForst_Lite/Data
  sudo cp $SOURCE_DIR/Forms/* $STAGING_DIR/GForst_Lite/Forms
- sudo cp $SOURCE_DIR/Forms/* $STAGING_DIR/GForst_Lite/Symbology
+ sudo cp $SOURCE_DIR/Symbology/* $STAGING_DIR/GForst_Lite/Symbology
   
 sudo cp $SOURCE_DIR/Maps/GForst_ME.qgs $STAGING_DIR/GForst_ME/Maps
 sudo cp $SOURCE_DIR/Forms/* $STAGING_DIR/GForst_ME/Forms
-sudo cp $SOURCE_DIR/Forms/* $STAGING_DIR/GForst_ME/Symbols
+sudo cp $SOURCE_DIR/Symbology/* $STAGING_DIR/GForst_ME/Symbols
  
   echo "Zipping the files..."
  # Create a zip package.
@@ -43,13 +43,21 @@ sudo cp $SOURCE_DIR/Forms/* $STAGING_DIR/GForst_ME/Symbols
  sudo zip -ru $STAGING_DIR/gf_deployment/packages/$PACKAGE_VERSION/GForst_Lite_v$PACKAGE_VERSION.zip  ./GForst_Lite/ 
  sudo zip -ru $STAGING_DIR/gf_deployment/packages/$PACKAGE_VERSION/GForst_ME_v$PACKAGE_VERSION.zip ./GForst_ME/ 
  
-
  # Update date on web page.
  CURR_DATE=$(date +"%m-%d-%Y")
  sudo sed -i 's/\[DATE\]/'$CURR_DATE/g  $STAGING_DIR/gf_deployment/index.html
  
- # Copy files to web directory.
-sudo cp -rf $STAGING_DIR/gf_deployment/* $WEB_DIR
+# Copy files to web directory.
+ sudo cp -rf $STAGING_DIR/gf_deployment/* $WEB_DIR
+
+
+ echo "Packaging and deployment complete."
  
- echo "Packaging and deploying complete."
+ echo "Deploying Django
+sudo cp -fr $SOURCE_DIR/geoweb/ $WEB_DIR/geoweb
+sudo python $WEB_DIR/geoweb/manage.py collectstatic
+sudo rm -f $WEB_DIR/geoweb/geoweb/settings.py
+sudo cp $WEB_DIR/geoweb/geoweb/settings_prod.py $WEB_DIR/geoweb/geoweb/settings.py 
+sudo service apache2 stop
+sudo service apache2 start
  
