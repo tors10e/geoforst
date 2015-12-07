9cbd2cea06b0795500cc516ad21be013c102d767
Torsten Ernst <tors10e@users.noreply.github.com>
2015-12-07 17:42:08 +1000

    Added lumber scaling app and update scaling ticket and sawmills to use bootstrap3.

---
+++ geoweb/lumber_scaling/filters.py
@@ -0,0 +1,47 @@
+# Django modules
+import django_filters
+from django_filters import ChoiceFilter, DateFilter, IsoDateTimeFilter
+from django.db.models.query import QuerySet
+
+# Django apps
+from lumber_scaling.models import *
+
+class LumberLoadFilter(django_filters.FilterSet):
+           
+    class Meta:
+        model = LumberLoad
+        fields = {
+                  'load_date':['exact'],
+                  }
+
+    def __init__(self, *args, **kwargs):
+         self.user = kwargs.pop('current_user')        
+         super(LumberLoadFilter, self).__init__(*args, **kwargs)
+         
+class SawmillFilter(django_filters.FilterSet):
+           
+    class Meta:
+        model = Sawmill
+        fields = {
+                  'name':['exact'],
+                  }
+
+    def __init__(self, *args, **kwargs):
+         self.user = kwargs.pop('current_user')        
+         super(SawmillFilter, self).__init__(*args, **kwargs)
+         
+class ScalingTicketFilter(django_filters.FilterSet):
+           
+    class Meta:
+        model = ScalingTicket
+        fields = {
+                  'sawmill':['exact'],
+                  }
+
+    def __init__(self, *args, **kwargs):
+         self.user = kwargs.pop('current_user')        
+         super(ScalingTicketFilter, self).__init__(*args, **kwargs)
+
+
+
+   
---
+++ geoweb/lumber_scaling/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, url
+from lumber_scaling import views
+ 
+urlpatterns = patterns('',
+    url(r'^$', views.LumberScaling, name = 'lumber-scaling'),
+    
+    url(r'^lumber_load/$', views.LumberLoadFilterList, name='lumber-load-list'),
+    url(r'^lumber_load/add/$', views.LumberLoadCreate.as_view(), name='lumber-load-add'),
+    url(r'^lumber_load/(?P<pk>\d+)/edit/$', views.LumberLoadUpdate.as_view(), name='lumber-load-edit'),
+    url(r'^lumber_load/(?P<pk>\d+)/delete/$', views.LumberLoadDelete.as_view(), name='lumber-load-delete'),
+    url(r'^lumber_load/(?P<pk>\d+)/detail/$', views.LumberLoadDetail.as_view(), name='lumber-load-detail'),
+    url(r'^lumber_load/(?P<pk>\d+)/addlog/$', views.LogDataCreate.as_view(), name='log-data-add'),
+    
+    url(r'^log_data/(?P<pk>\d+)/edit/$', views.LogDataUpdate.as_view(), name='log-data-edit'),
+    url(r'^log_data/(?P<pk>\d+)/delete/$', views.LogDataDelete.as_view(), name='log-data-delete'),
+    
+    url(r'^scaling_ticket/add/$', views.ScalingTicketCreate.as_view(), name='scaling-ticket-add'),
+    url(r'^scaling_ticket/$', views.ScalingTicketList, name='scaling-ticket-list'),
+    url(r'^scaling_ticket/(?P<pk>\d+)/edit/$', views.ScalingTicketUpdate.as_view(), name='scaling-ticket-edit'),
+    url(r'^scaling_ticket/(?P<pk>\d+)/delete/$', views.ScalingTicketDelete.as_view(), name='scaling-ticket-delete'),
+    
+    url(r'^sawmill/add/$', views.SawmillCreate.as_view(), name='sawmill-add'),
+    url(r'^sawmill/$', views.SawmillFilterList, name='sawmill-list'),
+    url(r'^sawmill/(?P<pk>\d+)/edit/$', views.SawmillUpdate.as_view(), name='sawmill-edit'),
+    url(r'^sawmill/(?P<pk>\d+)/delete/$', views.SawmillDelete.as_view(), name='sawmill-delete'),
+)
