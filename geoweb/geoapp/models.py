db310f8c4862e2a7df22ff1fe68f078de95cf353
torstenernst <tors10e@users.noreply.github.com>
2015-12-10 17:53:07 +1000

    Altered deployment scripts.

--- geoweb/geoapp/models.py
+++ geoweb/geoapp/models.py
@@ -5,18 +5,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.gis.geos import Point
 import uuid
 
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#     * Rearrange models' order
-#     * Make sure each model has one field with primary_key=True
-# Feel free to rename the models, but don't rename db_table values or field names.
-#
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
-
-# Returns the string representation of the model.
-
-
 class BurnCompartment(models.Model):
     burncompartment_id = models.AutoField(primary_key=True)
     geometry = models.TextField(blank=True) # This field type is a guess.
