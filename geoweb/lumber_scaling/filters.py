44c94dba3e3280d773fa86528c44aeca0707df86
torstenernst <tors10e@users.noreply.github.com>
2015-12-21 22:31:17 +1000

    Changed sawmill filters to use distinct choice lists with fields that are not foreign key relations.

--- geoweb/lumber_scaling/filters.py
+++ geoweb/lumber_scaling/filters.py
@@ -1,10 +1,11 @@
 # Django modules
-import django_filters
-from django_filters import ChoiceFilter, DateFilter, IsoDateTimeFilter
+import django_filters 
+from django_filters import ChoiceFilter, DateFilter, IsoDateTimeFilter, MethodFilter, FilterSet
 from django.db.models.query import QuerySet
 
 # Django apps
 from lumber_scaling.models import *
+from django_filters.filters import ModelChoiceFilter
 
 class LumberLoadFilter(django_filters.FilterSet):
            
@@ -17,17 +18,18 @@ class LumberLoadFilter(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):      
          super(LumberLoadFilter, self).__init__(*args, **kwargs)
          
-class SawmillFilter(django_filters.FilterSet):
-           
+class SawmillFilter(django_filters.FilterSet):         
+    name = django_filters.AllValuesFilter(lookup_type='exact', distinct=True)
+    state = django_filters.AllValuesFilter(lookup_type='exact', distinct=True)
+    city = django_filters.AllValuesFilter(lookup_type='exact', distinct=True)
     class Meta:
         model = Sawmill
-        fields = {
-                  'name':['exact'],
-                  }
+        fields =['name', 'city', 'state']
 
     def __init__(self, *args, **kwargs):       
-         super(SawmillFilter, self).__init__(*args, **kwargs)
-         
+         super(SawmillFilter, self).__init__(*args, **kwargs)     
+
+    
 class ScalingTicketFilter(django_filters.FilterSet):
            
     class Meta:
