83d0915a5937d87c147c75ac6f10694c98741ae6
Torsten Ernst <tors10e@users.noreply.github.com>
2016-02-13 12:33:28 +1000

    Moved static files to project root.

--- geoweb/geoweb/static/geoweb/js/PlannedActivity.js
+++
@@ -1,5 +0,0 @@
-// When document is ready...
-$(function() {
-	// Set planned year filter value default to current year.
-	document.getElementById("id_planned_year").value = new Date().getFullYear();
-});	
