1e7eae869509847a39f12482682788a208440789
Torsten Ernst <tors10e@users.noreply.github.com>
2016-02-13 12:31:41 +1000

    Changed settings file for production to use new geoforst home directory to pull static files.

--- geoweb/geoweb/settings_prod.py
+++ geoweb/geoweb/settings_prod.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/var/www/geoweb/media/'
+MEDIA_ROOT = '/var/www/geoforst.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -62,8 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/var/www/geoforst.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -75,7 +74,7 @@ LOGOUT_REDIRECT_URL = '/home/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-   "/var/www/geoweb/static",
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
